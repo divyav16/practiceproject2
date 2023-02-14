e453da3d6bd053bc287bfc3e5db7b68badab7902
divya vandra <divyavandra@gmail.com>
2023-02-14 22:45:53 +0000

    second version

---
+++ manualtopics.java
@@ -0,0 +1,25 @@
+package org.example.manual;
+
+public class manualtopics {
+
+    public static void main( String[] args ){
+
+    System.out.println( "Smoke Testing" );
+    System.out.println("Sanity Testing");
+    System.out.println("Regression Testing");
+    System.out.println("Maintenance Testing");
+    System.out.println("Acceptance Testing");
+    System.out.println("Alpha Testing");
+    System.out.println("Beta Testing");
+    System.out.println("Adhoc Testing");
+    System.out.println("Functional Testing");
+    System.out.println("Non Functional Testing");
+}
+
+
+        // package name has been change from manual to manual topic
+        /* i have written different testing types.
+        there are few more as well.
+
+         */
+}
